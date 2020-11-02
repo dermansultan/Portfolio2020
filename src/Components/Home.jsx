d0d4c6f2eeacd1eb4b1501e24d312932447b8173
dermansultan <abdurrehmansultan4@gmail.com>
2020-11-02 14:55:24 -0500

    Hero Animation added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
         <div className='heroContainer'>
             <div className='greetingBio'>
             
-                <h1>Derman.greet();</h1>
-                <p>I'm a <span>UX/UI Designer</span> with a passion for creating elegeant and intiuitive experiences through <span>Design</span> and <span>Code</span>.</p>
+                <h1 className='greetingMsg'>Derman.greet();</h1>
+                <p className='greetingPara'>I'm a <span>UX/UI Designer</span> with a passion for creating elegeant and intiuitive experiences through <span>Design</span> and <span>Code</span>.</p>
             </div>
             <img className='landingGraphic' src={landingGraphic}/>
         </div>

d8197f54a526ae807e72e2707bb8c6f78a994a9d
dermansultan <abdurrehmansultan4@gmail.com>
2020-11-04 12:28:05 -0500

    Arrow CSS animations updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,11 +6,12 @@ const Home = () => {
         <div className='homeContainer'>
         <div className='heroContainer'>
             <div className='greetingBio'>
-                <h1 className='greetingMsg'>Derman.greet();</h1>
+                <h1 className='greetingMsg'>Derman.greeting();</h1>
                 <p className='greetingPara'>I'm a <span style={{color:"#1D66B9", fontWeight: 500}}>UX/UI Designer</span> with a passion for creating elegant and intuiitive experiences through <span style={{color:"#1DB954", fontWeight: 500}}>Design</span> and <span style={{color:"#1D66B9", fontWeight: 500}}>Code</span>.</p>
             </div>
             <img className='landingGraphic' src={landingGraphic}/>
         </div>
+        <i className="arrow down"></i>
         </div>
         
     )

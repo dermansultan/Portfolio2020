bf77831e58ef75de42da8730553b9699ee951609
dermansultan <abdurrehmansultan4@gmail.com>
2020-11-05 14:14:10 -0500

    Viewport fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,9 +9,18 @@ const Home = () => {
                 <h1 className='greetingMsg'>Derman.greeting();</h1>
                 <p className='greetingPara'>I'm a <span style={{color:"#1D66B9", fontWeight: 500}}>UX/UI Designer</span> with a passion for creating elegant and intuiitive experiences through <span style={{color:"#1DB954", fontWeight: 500}}>Design</span> and <span style={{color:"#1D66B9", fontWeight: 500}}>Code</span>.</p>
             </div>
-            <img className='landingGraphic' src={landingGraphic}/>
+            <img draggable='false' className='landingGraphic' src={landingGraphic}/>
         </div>
         <i className="arrow down"></i>
+        <div className='projectsContainer'>
+        <h1 className='projectsHeader'>Projects</h1>
+        <div className='tilesContainer'>
+            <div className='tile'></div>
+            <div className='tile'></div>
+            <div className='tile'></div>
+            <div className='tile'></div>
+        </div>
+        </div>
         </div>
         
     )

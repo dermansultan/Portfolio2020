f9df15da114bb632eb8e31a183f24c5715005faf
dermansultan <abdurrehmansultan4@gmail.com>
2020-11-05 21:24:15 -0500

    Responsive Tile Update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import landingGraphic from '../Img/CodePenSquares.png'
+import rumaBanner from '../Img/Ruma4k.png'
 
 const Home = () => {
     return(
@@ -7,17 +8,17 @@ const Home = () => {
         <div className='heroContainer'>
             <div className='greetingBio'>
                 <h1 className='greetingMsg'>Derman.greeting();</h1>
-                <p className='greetingPara'>I'm a <span style={{color:"#1D66B9", fontWeight: 500}}>UX/UI Designer</span> with a passion for creating elegant and intuiitive experiences through <span style={{color:"#1DB954", fontWeight: 500}}>Design</span> and <span style={{color:"#1D66B9", fontWeight: 500}}>Code</span>.</p>
+                <p className='greetingPara'>I'm a <span style={{color:"#1D66B9", fontWeight: 500}}>UX/UI Designer</span> with a passion for creating elegant and intuitive experiences through <span style={{color:"#1DB954", fontWeight: 500}}>Design</span> and <span style={{color:"#1D66B9", fontWeight: 500}}>Code</span>.</p>
             </div>
             <img draggable='false' className='landingGraphic' src={landingGraphic}/>
         </div>
         <div className='projectsContainer'>
         <h1 className='projectsHeader'>Projects</h1>
         <div className='tilesContainer'>
-            <div className='tile'></div>
-            <div className='tile'></div>
-            <div className='tile'></div>
-            <div className='tile'></div>
+            <img alt='RumaLogoBanner' draggable='false' className='tile' src={rumaBanner}/>
+            <img alt='RumaLogoBanner' draggable='false' className='tile' src={rumaBanner}/>
+            <img alt='RumaLogoBanner' draggable='false' className='tile' src={rumaBanner}/>
+            <img alt='RumaLogoBanner' draggable='false' className='tile' src={rumaBanner}/>
         </div>
         </div>
         </div>

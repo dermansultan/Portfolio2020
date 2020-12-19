139ad6917a610593839288f118a2438942f8f118
dermansultan <abdurrehmansultan4@gmail.com>
2020-12-19 09:35:03 -0500

    Case Study Components

---
+++ src/Components/CaseStudy/ProjectSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectSection(props) {
+  return (
+  <div className='projectSectionWrapper'>
+    
+  </div>
+  );
+}
+
+export default ProjectSection
---
+++ src/Components/CaseStudy/ProjectSub/ProjectBody.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProjectBody(props) {
+  return (
+<p className='projectPara'>{props.text}</p>
+  );
+}
+
+export default ProjectBody

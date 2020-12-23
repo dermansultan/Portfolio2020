eb149f6202ac6eab4bec049f2b076c5cb841e69b
Derman Sultan <abdurrehmansultan4@gmail.com>
2020-12-23 12:49:02 -0500

    ruma changes

--- src/Components/RumaCaseStudy.jsx
+++ src/Components/RumaCaseStudy.jsx
@@ -30,14 +30,20 @@ const Ruma = () => {
         <div className='projectSectionWrapper' style={{paddingTop:"12px"}}>
           <ProjectHeader text='Solution'></ProjectHeader>
           <ProjectParagraph text='A web app that encourages users to  explore other content based on their interests, and watch content amongst their loved ones. '></ProjectParagraph>
+          <ProjectTiles csTitle='Ruma'></ProjectTiles>
         </div>
-        <div className='projectSectionWrapper' style={{paddingTop:"12px"}}>
-        <ProjectHeader text='Solution'></ProjectHeader>
-        <ProjectTiles csTitle='Ruma'></ProjectTiles>
-        </div>
-        <div className='projectSectionWrapper' style={{paddingTop:"12px"}}>
-        <ProjectHeader text='Timeline'></ProjectHeader>
+        <div className='projectSectionWrapper' style={{paddingTop:"12px", display:"flex"}}>
+        <ProjectHeader text='Project Timeline'></ProjectHeader>
         <img classname='projectTimeline Ruma'alt='rumaTimeline' src={timelineRuma}></img>
+        </div>
+        <div className='projectSectionWrapper' style={{paddingTop:"12px"}}>
+          <ProjectHeader text='User Thinking: Audience and the Brief'></ProjectHeader>
+          <ProjectParagraph text='With the short amount of time to create a prototype my partner and I researched existing applications that allowed users to share the watching experience. We looked into websites such as Watch2gether and third party browser plugins such as Netflix Party. 
+
+For inspiration on the layout we looked upon current streaming platforms such as Netflix, Disney+ and Apple TV, and Amazon Prime Video. We wanted to make sure the experience we created would be familiar to users who are accustomed to streaming platforms interfaces.
+
+The brief given to us also included many different questions to consider, we did our best to address these within the ideation process of the application. These specifically were some questions from the brief that we had considered in our ideation process. 
+'></ProjectParagraph>
         </div>
         <div className='deadspace' style={{height:'900px'}}></div>
       </div>

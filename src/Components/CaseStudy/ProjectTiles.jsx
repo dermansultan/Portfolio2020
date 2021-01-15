16ac1195960b8ccc1358b3187a1a5c6bb9a0ca03
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-15 16:51:16 -0500

    lightbox css

--- src/Components/CaseStudy/ProjectTiles.jsx
+++ src/Components/CaseStudy/ProjectTiles.jsx
@@ -2,7 +2,7 @@ const ProjectTile = (props) => {
   const images = props.images;
   const imagesList = images.map((image) => (
     <img
-      key={image.toString()}
+      key={image.id}
       src={image.src}
       onClick={() => {
         props.lightBox(image.src, image.description);

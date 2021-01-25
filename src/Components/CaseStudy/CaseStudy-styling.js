a0418ed322dab910e3117a3de56dc9e6dae2a9b1
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-25 16:49:29 -0500

    styled component update

--- src/Components/CaseStudy/CaseStudy-styling.js
+++ src/Components/CaseStudy/CaseStudy-styling.js
@@ -1,8 +1,29 @@
 import styled from "styled-components";
 
-// Components to Replace: 
-// - Project Banner 
-// - Project Body
-// - Project Header
-// - Project Paragraph
+export const Banner = styled.div`
+  backgroundimage: url(${(props) => props.backImg});
+`;
+
+export const BodyPara = styled.p`
+  font-weight: 400;
+  padding-top: 12px;
+  line-height: 150%;
+  font-size: clamp(14px, 1.5vw, 18px);
+  color: var(--second-txt-color);
+`;
+
+export const Header = styled.h1`
+  font-size: clamp(30px, 1.5vw, 55px);
+  color: var(--main-txt-colour);
+`;
+
+export const WrapperCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
--- src/Components/CaseStudy/ProjectDetails.jsx
+++ src/Components/CaseStudy/ProjectDetails.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``
 
 // Recreate with styled-component
 

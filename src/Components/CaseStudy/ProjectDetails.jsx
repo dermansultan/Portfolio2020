f4b19d25d84e5dfcfc7ef8b07afb7a313f8e9abd
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-25 20:43:10 -0500

    ProjectDetails converted over to styled-comp

--- src/Components/CaseStudy/ProjectDetails.jsx
+++ src/Components/CaseStudy/ProjectDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { WrapperCol } from "./CaseStudy-styling";
 
 const DetailsWrapper = styled.div`
   display: flex;
@@ -33,33 +34,80 @@ const ProtoBtn = styled.button`
   }
 `;
 
+const DetailsTitle = styled.h1`
+  color: var(--main-txt-colour);
+  white-space: nowrap;
+  font-weight: 500;
+  font-size: clamp(10px, 1.5vw, 22px);
+`;
+
+const DetailsPara = styled.p`
+  color: var(--second-txt-color);
+  white-space: nowrap;
+  font-size: clamp(8px, 1.5vw, 18px);
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  width: 50%;
+  justify-content: space-between;
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    margin-top: 20px;
+  }
+`;
+
 // Recreate with styled-component
 
 function ProjectDetails(props) {
   return (
-    <div className="projectDetailsWrapper">
+    <DetailsWrapper>
       <a href={props.demoLink}>
-        <button className="protoBtn">Live Prototype</button>
+        <ProtoBtn></ProtoBtn>
       </a>
-      <div className="projectDetails">
-        <div className="details">
-          <p className="deTitle">My Role</p>
-          <p className="deSub">{props.role}</p>
-        </div>
-        <div className="details">
-          <p className="deTitle">Tools</p>
-          <p className="deSub">{props.tools}</p>
-        </div>
-        <div className="details">
-          <p className="deTitle">Team</p>
-          <p className="deSub">{props.team}</p>
-        </div>
-        <div className="details">
-          <p className="deTitle">Duration</p>
-          <p className="deSub">{props.duration}</p>
-        </div>
-      </div>
-    </div>
+      <ContentWrapper>
+        <WrapperCol>
+          <DetailsTitle>My Role</DetailsTitle>
+          <DetailsPara>{props.role}</DetailsPara>
+        </WrapperCol>
+        <WrapperCol>
+          <DetailsTitle>Tools</DetailsTitle>
+          <DetailsPara>{props.tools}</DetailsPara>
+        </WrapperCol>
+        <WrapperCol>
+          <DetailsTitle>Team</DetailsTitle>
+          <DetailsPara>{props.team}</DetailsPara>
+        </WrapperCol>
+        <WrapperCol>
+          <DetailsTitle>Duration</DetailsTitle>
+          <DetailsPara>{props.duration}</DetailsPara>
+        </WrapperCol>
+      </ContentWrapper>
+    </DetailsWrapper>
+
+    // <div className="projectDetailsWrapper">
+    //   <a href={props.demoLink}>
+    //     <button className="protoBtn">Live Prototype</button>
+    //   </a>
+    //   <div className="projectDetails">
+    //     <div className="details">
+    //       <p className="deTitle">My Role</p>
+    //       <p className="deSub">{props.role}</p>
+    //     </div>
+    //     <div className="details">
+    //       <p className="deTitle">Tools</p>
+    //       <p className="deSub">{props.tools}</p>
+    //     </div>
+    //     <div className="details">
+    //       <p className="deTitle">Team</p>
+    //       <p className="deSub">{props.team}</p>
+    //     </div>
+    //     <div className="details">
+    //       <p className="deTitle">Duration</p>
+    //       <p className="deSub">{props.duration}</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 

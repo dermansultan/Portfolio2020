ac78085c0ec550ce033fb568c20e3dde65b3bcce
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-30 18:21:12 -0500

    Ruma updates

--- src/Components/RumaCaseStudy.jsx
+++ src/Components/RumaCaseStudy.jsx
@@ -11,7 +11,14 @@ import ProjectImgCap from "../Components/CaseStudy/ProjectImgCap";
 import { motion } from "framer-motion";
 import { docTitleUpdate } from "./docTitleUpdate";
 import { images, solutionArr } from "../Img/caseStudies/Ruma/Image";
-import { CaseContainer, Banner, BodyPara, Header, WrapperCol, Section } from "./CaseStudy/CaseStudy-styling";
+import {
+  CaseContainer,
+  Banner,
+  BodyPara,
+  Header,
+  WrapperCol,
+  Section,
+} from "./CaseStudy/CaseStudy-styling";
 
 //Create master Image module for img imports
 const outro = { duration: 1.45, ease: [0.6, 0.01, -0.05, 0.9] };
@@ -29,7 +36,7 @@ const Ruma = ({ lightBoxHandler }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-      <Banner backImg={rumaBanner}></Banner>
+        <Banner backImg={rumaBanner}></Banner>
         <ProjectDetails
           role={"UX/UI Design, Branding"}
           tools={"Invision, Adobe XD"}
@@ -38,22 +45,30 @@ const Ruma = ({ lightBoxHandler }) => {
         ></ProjectDetails>
         <Section>
           <Header>Background</Header>
-          <BodyPara>"I took part in the Adobe x Netflix Creative Jam, a 48 hour online design challenge. My partner and I came up with our project Ruma. Ruma is a desktop app that aims to give families and friends a way to enjoy content together remotely."</BodyPara>
+          <BodyPara>
+            "I took part in the Adobe x Netflix Creative Jam, a 48 hour online
+            design challenge. My partner and I came up with our project Ruma.
+            Ruma is a desktop app that aims to give families and friends a way
+            to enjoy content together remotely."
+          </BodyPara>
         </Section>
         <Section>
-        <Header>Design Challenge</Header>
+          <Header>Design Challenge</Header>
           <ProjectQuote text='"Design a third-party desktop web site or desktop app (not Netflix-branded) where the community can celebrate shared stories together through communal and authentic experiences."'></ProjectQuote>
         </Section>
-        <div className="projectSectionWrapper" style={{ paddingTop: "12px" }}>
-          <ProjectHeader text="Solution"></ProjectHeader>
-          <ProjectParagraph text="A web app that encourages users to  explore other content based on their interests, and watch content amongst their loved ones. "></ProjectParagraph>
+        <Section>
+          <Header>Solution</Header>
+          <BodyPara>
+            A web app that encourages users to explore other content based on
+            their interests, and watch content amongst their loved ones.
+          </BodyPara>
           <ProjectTiles
             lightBox={lightBoxHandler}
             images={solutionArr}
           ></ProjectTiles>
-        </div>
-        <div className="projectSectionWrapper" style={{ paddingTop: "12px" }}>
-          <ProjectHeader text="Project Timeline"></ProjectHeader>
+        </Section>
+        <Section>
+          <Header>Project Timeline</Header>
           <img
             className="projectTimeline Ruma"
             alt="rumaTimeline"
@@ -62,18 +77,24 @@ const Ruma = ({ lightBoxHandler }) => {
               lightBoxHandler(timelineRuma, "This was a timeline!");
             }}
           ></img>
-        </div>
-        <div className="projectSectionWrapper" style={{ paddingTop: "12px" }}>
-          <ProjectHeader text="User Thinking: Audience and the Brief"></ProjectHeader>
-          <ProjectParagraph
-            text="With the short amount of time to create a prototype my partner and I researched existing applications that allowed users to share the watching experience. We looked into websites such as Watch2gether and third party browser plugins such as Netflix Party. 
-
-For inspiration on the layout we looked upon current streaming platforms such as Netflix, Disney+ and Apple TV, and Amazon Prime Video. We wanted to make sure the experience we created would be familiar to users who are accustomed to streaming platforms interfaces.
-
-The brief given to us also included many different questions to consider, we did our best to address these within the ideation process of the application. These specifically were some questions from the brief that we had considered in our ideation process. 
-"
-          ></ProjectParagraph>
-        </div>
+        </Section>
+        <Section>
+          <Header>User Thinking: Audience and the Brief</Header>
+          <BodyPara>
+            With the short amount of time to create a prototype my partner and I
+            researched existing applications that allowed users to share the
+            watching experience. We looked into websites such as Watch2gether
+            and third party browser plugins such as Netflix Party. For
+            inspiration on the layout we looked upon current streaming platforms
+            such as Netflix, Disney+ and Apple TV, and Amazon Prime Video. We
+            wanted to make sure the experience we created would be familiar to
+            users who are accustomed to streaming platforms interfaces. The
+            brief given to us also included many different questions to
+            consider, we did our best to address these within the ideation
+            process of the application. These specifically were some questions
+            from the brief that we had considered in our ideation process.
+          </BodyPara>
+        </Section>
         <div className="projectSectionWrapper RumaBvS">
           <div className="leftContainer">
             <h2 className="userThinking Brief">Brief</h2>
